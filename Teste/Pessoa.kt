4c23587b294d097e1aea5a72fe0297fbb7617da4
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-06-11 01:06:24 -0300

    Implementando classe privada

--- Teste/Pessoa.kt
+++ Teste/Pessoa.kt
@@ -1,19 +1,32 @@
 package Teste
 
+//Classe
 class Pessoa { 
     var nome: String = "Abrovildo Pinto Roxo"
-    var cpf: String = "010.010.010.00"
-    var endereco: String = "Copacabana Palace"
+    private set //private é somente para dentro da classe
+    var cpf: String = "010.010.010.00"    
+    var idade: Int = 18
+    inner class Endereco {
+        var logradouro: String = "Rua dos Bobos"
+        var numero: Int = 0
+        var bairro: String = "Centro"
+        var cidade: String = "São Paulo"
+        var uf: String = "SP"
+    }
+
     var telefone: String = ""
     var email: String = ""
     var senha: String = ""
-    var saldo: Double = 0.0    
+    var saldo: Double = 0.0
+    
 }
 
 fun main() {
-    val jether = Pessoa()
+    val jether = Pessoa() //Instanciação da classe(Objeto)
 
+    println(jether)//Imprime o objeto e referencia de memória
+    //jether.nome = "Jether"
     println(jether.nome)
-    println(jether.cpf)
-    println(jether.endereco)
+    println("CPF: " +jether.cpf)
+    println("Endereco: " + jether.Endereco().logradouro)
 }
